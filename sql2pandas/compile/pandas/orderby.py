670bbe008db7936f9dcd2e0e91b9a28cabdaefd2
Eugene Wu <ewu@cs.columbia.edu>
2022-05-18 10:43:13 -0400

    lazy commit msg

--- sql2pandas/compile/pandas/orderby.py
+++ sql2pandas/compile/pandas/orderby.py
@@ -1,5 +1,5 @@
 import json
-from ...ops import GroupBy
+from ...context import Context
 from ..translator import *
 from ..orderby import *
 from .translator import *
@@ -32,21 +32,24 @@ class PandasOrderByTopTranslator(OrderByTopTranslator, PandasTranslator):
     # a query can order by the results of expressions.
     # we first evaluate each of the expressions into temporary
     # columns, sort by those columns, and then drop them
+    ascdescs = self.bottom.op.ascdescs
     aliases = []
+    kwargs = {}
     for i, e in enumerate(self.compile_exprs(ctx, self.op.order_exprs, v_in)):
       aliases.append(ctx.new_var("_orderkey_"))
-      if self.bottom.op.ascdescs[i] != "asc":
-        e = "-{e}".format(e=e)
-      ctx.add_line("{df}['{alias}'] = {e}",
-          df = v_in,
-          alias=aliases[-1],
-          e=e)
+      e = "-"+e if ascdescs[i] != "asc" else e
+      kwargs[aliases[-1]] = e
+
+    tmpdf = (Context()
+        .func("{df}.assign", [], kwargs, df=v_in)
+        .compiler.compile())
     
     # call DataFrame's sort, then drop the tmp columns
-    ctx.add_line("{outdf} = {df}.sort_values({keys}).drop({keys}, axis=1)",
-        outdf=v_outdf,
-        df=v_in,
-        keys=json.dumps(aliases))
+    kwargs = dict(outdf=v_outdf, tmpdf=tmpdf, keys=json.dumps(aliases))
+    with ctx.indent("{outdf} = ({tmpdf}", **kwargs):
+      ctx.add_lines([
+        ".sort_values({keys})",
+        ".drop({keys}, axis=1))"], **kwargs)
 
     ctx['df'] = v_outdf
     self.parent_translator.consume(ctx)
